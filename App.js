85b134a1eea1bd8b32a245bfef1e89e9d2173a5c
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-04-30 18:51:51 +0530

    Add a task list via Flatlist

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import React, {useState} from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [tasks, setTasks] = useState([
+        { task: "HTML I", done: true, id: 1 },
+        { task: "CSS", done: true, id: 2 },
+        { task: "Responsive design", done: true, id: 3 },
+    ]);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.content}>
+                <View style={styles.list}>
+                    <FlatList
+                        data={tasks}
+                        renderItem={({ item }) => (
+							<Text>{item.task}</Text>
+						)}
+                    />
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+	content: {
+		padding: 30,
+	},
+
+	list: {
+		marginTop: 30,
+	}
 });
